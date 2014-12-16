ae906c98a0ca47f437b8e266502c5a027da7311f
James Byers <jbyers@gmail.com>
2014-12-16 12:39:32 +0000

    constructor first

--- lib/resty/router/dns.lua
+++ lib/resty/router/dns.lua
@@ -17,6 +17,27 @@ local log_info = router.log_info
 local log_warn = router.log_warn
 local log_err = router.log_err
 
+function _M.new(self, opts)
+    local opts_dns = {
+        nameservers = {"127.0.0.1"},
+        retrans = 3,
+        timeout = 5000,
+    }
+    if opts.dns then
+        for k,v in pairs(opts.dns) do
+            opts_dns[k] = v
+        end
+    end
+    local resolver, err = dns:new(opts_dns)
+    if not resolver then
+        log_err("failed to instantiate resolver", err)
+    end
+    local self = {
+        resolver = resolver
+    }
+    return setmetatable(self, mt)
+end
+
 function _M.lookup(self, hostname)
     local answers, err = self.resolver:query(hostname)
     if not answers then
@@ -43,27 +64,6 @@ function _M.lookup(self, hostname)
         end
     end
     return routes
-end
-
-function _M.new(self, opts)
-    local opts_dns = {
-        nameservers = {"127.0.0.1"},
-        retrans = 3,
-        timeout = 5000,
-    }
-    if opts.dns then
-        for k,v in pairs(opts.dns) do
-            opts_dns[k] = v
-        end
-    end
-    local resolver, err = dns:new(opts_dns)
-    if not resolver then
-        log_err("failed to instantiate resolver", err)
-    end
-    local self = {
-        resolver = resolver
-    }
-    return setmetatable(self, mt)
 end
 
 return _M
